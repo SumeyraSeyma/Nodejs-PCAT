f1b0269b704c4307d79c310ba0ccc1ee4b70b3e9
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-11-18 00:22:05 +0300

    static files upload

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'photo1',
-    description: 'this is photo 1',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
