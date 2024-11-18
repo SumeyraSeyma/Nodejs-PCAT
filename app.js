3e7229e0298f26cff6bca8ee37591d6dace494c5
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-11-18 13:42:13 +0300

    ejs

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.get('/video-page', (req, res) => {
+  res.render('video-page');
 });
 
 const port = 3000;
