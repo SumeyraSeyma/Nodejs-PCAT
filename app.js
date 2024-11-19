2f57283562cf35368b7065a0d505351ba9f60b64
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-11-19 15:44:30 +0300

    mongoose connection

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Database Connection
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.json());//verileri json formatında almak için
+app.use(express.urlencoded({ extended: true }));//girilen verileri almak için
 
-app.get('/', (req, res) => {
-  res.render('index');
+//Routes
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', { photos });
 });
 
 app.get('/about', (req, res) => {
@@ -24,6 +33,11 @@ app.get('/add', (req, res) => {
 
 app.get('/video-page', (req, res) => {
   res.render('video-page');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
